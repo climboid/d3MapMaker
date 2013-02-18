a80c162d4eb1ae287f35b8383f9b27dde0bb4374
Oscar <oscar_villarreal-FIR@mckinsey.com>
2013-02-18 17:36:39 -0500

    uncommenting changes to main file

--- fetch.py
+++ fetch.py
@@ -256,96 +256,96 @@ symbol_dic = {
 
 
 
-# def find_key(dic, val):
-#     #return the key of dictionary dic given the value
-#     return [k for k, v in symbol_dic.iteritems() if v == val][0]
+def find_key(dic, val):
+    #return the key of dictionary dic given the value
+    return [k for k, v in symbol_dic.iteritems() if v == val][0]
 
-# # get the data
-# countryToCreate = raw_input('Country name? ')
-# # countryToCreate = countryToCreate.strip()
+# get the data
+countryToCreate = raw_input('Country name? ')
+# countryToCreate = countryToCreate.strip()
 
-# countryToCreate = countryToCreate[:1].upper() + countryToCreate[1:].lower()
-# fetchCountry = find_key(symbol_dic, countryToCreate)
-# fetchCountry = fetchCountry[:3]
+countryToCreate = countryToCreate.lower()
+fetchCountry = find_key(symbol_dic, countryToCreate)
 
-# url = "http://gadm.org/data/shp/"+fetchCountry+"_adm.zip"
+url = "http://gadm.org/data/shp/"+fetchCountry+"_adm.zip"
 
-# print url
 
-# file_name = url.split('/')[-1]
-# u = urllib2.urlopen(url)
-# f = open(file_name, 'wb')
-# meta = u.info()
-# file_size = int(meta.getheaders("Content-Length")[0])
-# print "Downloading: %s Bytes: %s" % (file_name, file_size)
+file_name = url.split('/')[-1]
+u = urllib2.urlopen(url)
+f = open(file_name, 'wb')
+meta = u.info()
+file_size = int(meta.getheaders("Content-Length")[0])
+print "Downloading: %s Bytes: %s" % (file_name, file_size)
 
-# file_size_dl = 0
-# block_sz = 8192
-# while True:
-#     buffer = u.read(block_sz)
-#     if not buffer:
-#         break
+file_size_dl = 0
+block_sz = 8192
+while True:
+    buffer = u.read(block_sz)
+    if not buffer:
+        break
 
-#     file_size_dl += len(buffer)
-#     f.write(buffer)
-#     status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-#     status = status + chr(8)*(len(status)+1)
-#     print status,
+    file_size_dl += len(buffer)
+    f.write(buffer)
+    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+    status = status + chr(8)*(len(status)+1)
+    print status,
 
-# f.close()
+f.close()
 
-# #unzip the files
-# zfile = zipfile.ZipFile(file_name)
+#unzip the files
+zfile = zipfile.ZipFile(file_name)
 
-# for name in zfile.namelist():
-#   (dirname, filename) = os.path.split(name)
-#   print "Decompressing " + filename + " on " + os.getcwd()
-#   fd = open(name,"w")
-#   fd.write(zfile.read(name))
-#   fd.close()
+for name in zfile.namelist():
+  (dirname, filename) = os.path.split(name)
+  print "Decompressing " + filename + " on " + os.getcwd()
+  fd = open(name,"w")
+  fd.write(zfile.read(name))
+  fd.close()
 
-# #convert file names to lower case this is needed for GDAL to work
-# for filename in os.listdir("."):
-#     os.rename(filename, filename.lower())
+#convert file names to lower case this is needed for GDAL to work
+for filename in os.listdir("."):
+    os.rename(filename, filename.lower())
 
-# #convert the shp file into geojson
-# toConvert = fetchCountry+'_adm1.shp'
-# os.system('/Library/Frameworks/GDAL.framework/Programs/ogr2ogr -f "GeoJSON" output.json '+toConvert)
+#convert the shp file into geojson
+toConvert = fetchCountry+'_adm1.shp'
 
-# #create the html file with all needed dependencies to make the map
-# finalFile = '<!doctype html>'
-# finalFile+='<html>'
-# finalFile+='<head>'
-# finalFile+='<meta charset="utf-8"/>'
-# finalFile+='<title></title>'
-# finalFile+='<script src="http://d3js.org/d3.v3.min.js"></script>'
-# finalFile+='</head>'
-# finalFile+='<body>'
-# finalFile+='<div id="mapContainer"></div>'
-# finalFile+='<script>'
-# finalFile+='var path, vis, xy;'
-# finalFile+='xy = d3.geo.mercator().scale(900);'
-# finalFile+='path = d3.geo.path().projection(xy);'
-# finalFile+='vis = d3.select("#mapContainer").append("svg:svg").attr("width", 960).attr("height", 600);'
-# finalFile+='d3.json("output.json", function(json) {'
-# finalFile+='return vis.append("svg:g").attr("class", "tracts").selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr("fill-opacity", 0.5).attr("fill", "#85C3C0").attr("stroke", "#222");'
-# finalFile+='});'
-# finalFile+='</script>'
-# finalFile+='</body>'
-# finalFile+='</html>'
 
-# def MakeFile(file_name):
+os.system('/Library/Frameworks/GDAL.framework/Programs/ogr2ogr -f "GeoJSON" output.json '+toConvert)
 
-#   temp_path = file_name
-#   file = open(temp_path, 'w')
-#   file.write(finalFile)
-#   file.close()
-#   print 'Execution completed. Map created successfully '
+#create the html file with all needed dependencies to make the map
+finalFile = '<!doctype html>'
+finalFile+='<html>'
+finalFile+='<head>'
+finalFile+='<meta charset="utf-8"/>'
+finalFile+='<title></title>'
+finalFile+='<script src="http://d3js.org/d3.v3.min.js"></script>'
+finalFile+='</head>'
+finalFile+='<body>'
+finalFile+='<div id="mapContainer"></div>'
+finalFile+='<script>'
+finalFile+='var path, vis, xy;'
+finalFile+='xy = d3.geo.mercator().scale(900);'
+finalFile+='path = d3.geo.path().projection(xy);'
+finalFile+='vis = d3.select("#mapContainer").append("svg:svg").attr("width", 960).attr("height", 600);'
+finalFile+='d3.json("output.json", function(json) {'
+finalFile+='return vis.append("svg:g").attr("class", "tracts").selectAll("path").data(json.features).enter().append("svg:path").attr("d", path).attr("fill-opacity", 0.5).attr("fill", "#85C3C0").attr("stroke", "#222");'
+finalFile+='});'
+finalFile+='</script>'
+finalFile+='</body>'
+finalFile+='</html>'
 
-# MakeFile('index.html')
+def MakeFile(file_name):
 
-# subprocess.Popen(['python', '-m', 'SimpleHTTPServer', '7777'])
-# webbrowser.open_new_tab('localhost:7777')
+  temp_path = file_name
+  file = open(temp_path, 'w')
+  file.write(finalFile)
+  file.close()
+  print 'Execution completed. Map created successfully '
+
+MakeFile('index.html')
+
+subprocess.Popen(['python', '-m', 'SimpleHTTPServer', '7777'])
+webbrowser.open_new_tab('localhost:7777')
 
 
 
